bf88404035c62e6f568d40d7355fc77aef805e03
Anthoni Mathias <63785517+Anthoni-Mathias@users.noreply.github.com>
2022-08-29 16:30:04 +0100

    create botao.css and index for botao file

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspesa from '../ListaSuspensa'
 import './Formulario.css'
@@ -19,6 +20,7 @@ const Formulario = () => {
           <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
           <CampoTexto label="Imagem" placeholder="Digite o endereco da imagem"/>
           <ListaSuspesa label="Time" itens={times}/>
+          <Botao texto="Criar Card"/>
         </form>
       </section>
   )
