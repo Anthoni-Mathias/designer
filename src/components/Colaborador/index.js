3ef0e724a3f4d3b1e5d241353988aff95a03deb1
Anthoni Mathias <63785517+Anthoni-Mathias@users.noreply.github.com>
2022-09-19 20:05:27 +0100

    create colabores time in the colaborado index create a rule to find name, image, and cargo when put the data

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,13 +1,13 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
+const Colaborador = ({ nome, imagem, cargo }) => {
   return (<div className='colaborador'>
       <div className='cabecalho'>
-        <img src='https://github.com/Anthoni-Mathias.png' alt='Anthoni Mathias'/>
+        <img src={imagem} alt={nome}/>
       </div>
       <div className='rodape'>
-        <h4>Anthoni Mathias</h4>
-        <h5>Front-End</h5>
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
       </div>
     </div>
   )
