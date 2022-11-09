18a5a455fa42cda6091f792bff67ac4a92e7b014
Anthoni Mathias <63785517+Anthoni-Mathias@users.noreply.github.com>
2022-11-09 15:10:51 +0000

    change path for image to designer

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -6,23 +6,23 @@ const Rodape = () => {
             <ul>
                 <li>
                     <a href="facebook.com" target="_blank">
-                        <img src="/image/fb.png" alt="facebook imagem" />
+                        <img src="/designer/image/fb.png" alt="facebook imagem" />
                     </a>
                 </li>
                 <li>
                     <a href="twitter.com" target="_blank">
-                        <img src="/image/tw.png" alt="twiter imagem" />
+                        <img src="/designer/image/tw.png" alt="twiter imagem" />
                     </a>
                 </li>
                 <li>
                     <a href="instagram.com" target="_blank">
-                        <img src="/image/ig.png" alt="instagram imagem" />
+                        <img src="/designer/image/ig.png" alt="instagram imagem" />
                     </a>
                 </li>
             </ul>
         </section>
         <section>
-            <img src="/image/logo.png" alt="logo imagen" />
+            <img src="/designer/image/logo.png" alt="logo imagen" />
         </section>
         <section>
             <p>
